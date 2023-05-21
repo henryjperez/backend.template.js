68409529344cb0a465a8da789c5ac48b0d36999c
Henry J. Perez <henryjosiasperezrangel@gmail.com>
2023-05-20 22:59:59 -0400

    some minor changes to the .env.example and user controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -6,23 +6,6 @@ const service = new UserServices();
 
 export class UserController {
 
-	static async login(userIdentifier: string, password, errorCallback?: (status?: number) => void) {
-		const isEmail = emailChecker(userIdentifier);
-		const findBy = isEmail ? "email" : "username";
-		const user = await service.find(findBy, userIdentifier);
-
-		if (!user) {
-			errorCallback?.();
-			throw new Error("Unauthorized");
-		}
-
-		const isMatchPassword = await service.verifyPassword(password, user.email);
-		if (!isMatchPassword) {
-			errorCallback?.();
-			throw new Error("Unauthorized");
-		}
-
-		return user;
-	}
+	
 
 }
