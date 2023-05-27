60ab677dbd3edb9234c29b4d61c7fc00b140fcb8
Henry J. Perez <henryjosiasperezrangel@gmail.com>
2023-05-27 17:39:45 -0400

    cleaning resolvers

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -8,24 +8,9 @@ import { getDogSchema, validateSchema } from "@dto";
 export type GqlContext = ReturnType<typeof buildContext<JWTPayload>>
 export const resolvers: Resolvers = {
 	Query: {
-		perro: async (a, b, c: GqlContext, d) => {
-			const user = await checkJwtGql(c);
-			console.log(user);
-			checkRolesGql(user, "user");
-			return "Perrito";
-		},
-		getDog: (_, args) => {
-			validateSchema(getDogSchema, args);
-			return `Perri llamado: ${args.name}`;
-		},
-		perriError: () => { throw new Error("Perrito Error") },
+		
 	},
 	Mutation: {
-		addPerro: (_, { dog }) => {
-			return {
-				name: dog.name,
-				id: dog.name + " " + dog.power,
-			}
-		}
+		
 	}
 };
