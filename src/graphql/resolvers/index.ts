a8f44b8d3a41ebc59737e588173022453d5a5e40
Henry J. Perez <henryjosiasperezrangel@gmail.com>
2023-10-07 18:03:20 -0400

    setting graphql endpoint, schemas and resolvers

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -4,14 +4,22 @@ import { checkJwtGql, checkRolesGql } from "@middlewares/auth.handler";
 import { JWTPayload, Resolvers } from "@interfaces";
 import { validateSchema } from "@dto";
 
-
+const db = {
+	users: [
+		{ email: "henryjosiasperezrangel@gmail.com", id: 0, name: "Henry J. Perez" },
+		{ email: "me@henryjperez.com", id: 1, name: "Henry J. Perez" },
+	]
+}
 export type GqlContext = ReturnType<typeof buildContext<JWTPayload>>
 export const resolvers: Resolvers = {
 	Query: {
-		user: () => ({ email: "developer@360integrations.io", id: 1, name: "Developer Name"}),
-		documents: () => ({ date: new Date().toDateString(), id: 1, name: "Top Secret" }),
+		user: (obj, args, context, info) => db.users[args.id],
+		users: () => db.users,
+		test: () => "This is a String to test the GQL Query",
+		moreTests: () => ({ name: "Another test", value: 800 }),
 	},
 	Mutation: {
-		setTest: () => "Set test string",
-	}
+		setTest: (obj, args, context, info) => "Set test string " + String(args.value),
+		setMoreTests: (obj, args, context, info) => "More testing strings " + args?.value,
+	},
 };
--- src/graphql/resolvers/types.d.ts
+++ src/graphql/resolvers/types.d.ts
@@ -6,6 +6,7 @@ export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: 
 export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
 export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
 export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
+export type RequireFields<T, K extends keyof T> = Omit<T, K> & { [P in K]-?: NonNullable<T[P]> };
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: { input: string; output: string; }
@@ -15,33 +16,39 @@ export type Scalars = {
   Float: { input: number; output: number; }
 };
 
-export type Documents = {
-  __typename?: 'Documents';
-  date?: Maybe<Scalars['String']['output']>;
-  id?: Maybe<Scalars['Int']['output']>;
-  name?: Maybe<Scalars['String']['output']>;
-};
-
 export type Mutation = {
   __typename?: 'Mutation';
+  setMoreTests?: Maybe<Scalars['String']['output']>;
   setTest?: Maybe<Scalars['String']['output']>;
 };
 
 
-export type MutationSetTestArgs = {
+export type MutationSetMoreTestsArgs = {
   value?: InputMaybe<Scalars['String']['input']>;
 };
 
-export type Pdf = {
-  __typename?: 'PDF';
-  name?: Maybe<Scalars['String']['output']>;
+
+export type MutationSetTestArgs = {
+  value: Scalars['Int']['input'];
 };
 
 export type Query = {
   __typename?: 'Query';
-  documents?: Maybe<Documents>;
-  pdf?: Maybe<Pdf>;
+  moreTests?: Maybe<Test>;
+  test?: Maybe<Scalars['String']['output']>;
   user?: Maybe<User>;
+  users?: Maybe<Array<Maybe<User>>>;
+};
+
+
+export type QueryUserArgs = {
+  id: Scalars['ID']['input'];
+};
+
+export type Test = {
+  __typename?: 'Test';
+  name?: Maybe<Scalars['String']['output']>;
+  value?: Maybe<Scalars['Int']['output']>;
 };
 
 export type User = {
@@ -124,47 +131,43 @@ export type DirectiveResolverFn<TResult = {}, TParent = {}, TContext = {}, TArgs
 /** Mapping between all available schema types and the resolvers types */
 export type ResolversTypes = ResolversObject<{
   Boolean: ResolverTypeWrapper<Scalars['Boolean']['output']>;
-  Documents: ResolverTypeWrapper<Documents>;
+  ID: ResolverTypeWrapper<Scalars['ID']['output']>;
   Int: ResolverTypeWrapper<Scalars['Int']['output']>;
   Mutation: ResolverTypeWrapper<{}>;
-  PDF: ResolverTypeWrapper<Pdf>;
   Query: ResolverTypeWrapper<{}>;
   String: ResolverTypeWrapper<Scalars['String']['output']>;
+  Test: ResolverTypeWrapper<Test>;
   User: ResolverTypeWrapper<User>;
 }>;
 
 /** Mapping between all available schema types and the resolvers parents */
 export type ResolversParentTypes = ResolversObject<{
   Boolean: Scalars['Boolean']['output'];
-  Documents: Documents;
+  ID: Scalars['ID']['output'];
   Int: Scalars['Int']['output'];
   Mutation: {};
-  PDF: Pdf;
   Query: {};
   String: Scalars['String']['output'];
+  Test: Test;
   User: User;
 }>;
 
-export type DocumentsResolvers<ContextType = any, ParentType extends ResolversParentTypes['Documents'] = ResolversParentTypes['Documents']> = ResolversObject<{
-  date?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
-  id?: Resolver<Maybe<ResolversTypes['Int']>, ParentType, ContextType>;
-  name?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
-  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
-}>;
-
 export type MutationResolvers<ContextType = any, ParentType extends ResolversParentTypes['Mutation'] = ResolversParentTypes['Mutation']> = ResolversObject<{
-  setTest?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType, Partial<MutationSetTestArgs>>;
-}>;
-
-export type PdfResolvers<ContextType = any, ParentType extends ResolversParentTypes['PDF'] = ResolversParentTypes['PDF']> = ResolversObject<{
-  name?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
-  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+  setMoreTests?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType, Partial<MutationSetMoreTestsArgs>>;
+  setTest?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType, RequireFields<MutationSetTestArgs, 'value'>>;
 }>;
 
 export type QueryResolvers<ContextType = any, ParentType extends ResolversParentTypes['Query'] = ResolversParentTypes['Query']> = ResolversObject<{
-  documents?: Resolver<Maybe<ResolversTypes['Documents']>, ParentType, ContextType>;
-  pdf?: Resolver<Maybe<ResolversTypes['PDF']>, ParentType, ContextType>;
-  user?: Resolver<Maybe<ResolversTypes['User']>, ParentType, ContextType>;
+  moreTests?: Resolver<Maybe<ResolversTypes['Test']>, ParentType, ContextType>;
+  test?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  user?: Resolver<Maybe<ResolversTypes['User']>, ParentType, ContextType, RequireFields<QueryUserArgs, 'id'>>;
+  users?: Resolver<Maybe<Array<Maybe<ResolversTypes['User']>>>, ParentType, ContextType>;
+}>;
+
+export type TestResolvers<ContextType = any, ParentType extends ResolversParentTypes['Test'] = ResolversParentTypes['Test']> = ResolversObject<{
+  name?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  value?: Resolver<Maybe<ResolversTypes['Int']>, ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
 }>;
 
 export type UserResolvers<ContextType = any, ParentType extends ResolversParentTypes['User'] = ResolversParentTypes['User']> = ResolversObject<{
@@ -175,10 +178,9 @@ export type UserResolvers<ContextType = any, ParentType extends ResolversParentT
 }>;
 
 export type Resolvers<ContextType = any> = ResolversObject<{
-  Documents?: DocumentsResolvers<ContextType>;
   Mutation?: MutationResolvers<ContextType>;
-  PDF?: PdfResolvers<ContextType>;
   Query?: QueryResolvers<ContextType>;
+  Test?: TestResolvers<ContextType>;
   User?: UserResolvers<ContextType>;
 }>;
 
