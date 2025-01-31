3e5f22e7ef47b6d7a49f2359da3f8dcd2310370a
Henry J. Perez <henryjosiasperezrangel@gmail.com>
2025-01-31 11:36:25 -0400

    changing 'bcrypt' to 'argon2' for password hashing

--- src/utils/password.handler.ts
+++ src/utils/password.handler.ts
@@ -1,11 +1,22 @@
-import bcrypt from "bcrypt";
+import argon2 from "argon2";
+// import bcrypt from "bcrypt";
 
-export async function hashPassword(password: string, salt = 10) {
-	const hash = await bcrypt.hash(password, salt);
+// export async function hashPassword(password: string, salt = 10) {
+// 	const hash = await bcrypt.hash(password, salt);
+// 	return hash;
+// }
+
+// export async function verifyPassword(password: string, hash: string) {
+// 	const isMatch = await bcrypt.compare(password, hash);
+// 	return isMatch;
+// }
+
+export async function hashPassword(password: string) {
+	const hash = await argon2.hash(password);
 	return hash;
 }
 
 export async function verifyPassword(password: string, hash: string) {
-	const isMatch = await bcrypt.compare(password, hash);
+	const isMatch = await argon2.verify(hash, password);
 	return isMatch;
 }
